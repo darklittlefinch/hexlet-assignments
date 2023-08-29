587c9ffb278118c1338d7b573c3fbb0e027d16b6
@hexlet/cli <support@hexlet.io>
2023-08-30 01:08:30 +0700

    submit java-web-ru/dynamic-routes

---
+++ java-web-ru/dynamic-routes/src/main/java/exercise/App.java
@@ -0,0 +1,61 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// BEGIN
+import io.javalin.http.NotFoundResponse;
+// END
+
+public final class App {
+
+    private static final List<Map<String, String>> COMPANIES = Data.getCompanies();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get("/companies/{id}", ctx -> {
+            var id = ctx.pathParam("id");
+
+            var companyId = "doesn't exist";
+            Map<String, String> companyMap = new HashMap<>();
+
+            for (Map<String, String> company: COMPANIES) {
+                if (company.get("id").equals(id)) {
+                    companyId = id;
+                    companyMap = company;
+                }
+            }
+
+            if (!companyId.equals("doesn't exist")) {
+                ctx.json(companyMap);
+            } else {
+                throw new NotFoundResponse("Company not found");
+            }
+        });
+        // END
+
+        app.get("/companies", ctx -> {
+            ctx.json(COMPANIES);
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("open something like (you can change id): /companies/5");
+        });
+
+        return app;
+
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
